b849220a78c2d655f0b342934cd4f13d98d1b19a
rohitnikam16 <rohitnikam162000@gmail.com>
2020-08-04 00:06:10 +0530

    Updates

---
+++ precache-manifest.b560c2a36942a4f1a3c8c788bab22496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb8781c5de8f8b139320ee52ccbf062",
+    "url": "/sorting_justifier/index.html"
+  },
+  {
+    "revision": "56b83634d7c7e62ec670",
+    "url": "/sorting_justifier/static/css/main.4b180853.chunk.css"
+  },
+  {
+    "revision": "c0b9582d0c2a95dc6983",
+    "url": "/sorting_justifier/static/js/2.f14cea11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sorting_justifier/static/js/2.f14cea11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b83634d7c7e62ec670",
+    "url": "/sorting_justifier/static/js/main.7401baac.chunk.js"
+  },
+  {
+    "revision": "ec68fd95909472915749",
+    "url": "/sorting_justifier/static/js/runtime-main.9269acfa.js"
+  }
+]);
